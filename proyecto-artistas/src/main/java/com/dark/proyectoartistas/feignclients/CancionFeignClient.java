522b02f8fc71d72285aab6f0ab5d5d49e87c076e
Carolina Atampis <carolinanatampis@gmail.com>
2022-07-13 12:43:05 -0300

    "Practica"

--- proyecto-artistas/src/main/java/com/dark/proyectoartistas/feignclients/CancionFeignClient.java
+++ proyecto-artistas/src/main/java/com/dark/proyectoartistas/feignclients/CancionFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name ="cancion-service")
-@RequestMapping("/cancion")
+@RequestMapping("/canciones")
 public interface CancionFeignClient {
 
     @PostMapping
