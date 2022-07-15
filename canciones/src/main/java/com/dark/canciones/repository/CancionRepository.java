efd74a1772951a47ffb4d8a4575813516aaf0c2e
Carolina Atampis <carolinanatampis@gmail.com>
2022-07-15 10:51:44 -0300

    "Practica con Gateway"

--- canciones/src/main/java/com/dark/canciones/repository/CancionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dark.canciones.repository;
-
-import com.dark.canciones.entity.Cancion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CancionRepository extends JpaRepository <Cancion, Long> {
-}
--- proyecto-artistas/src/main/java/com/dark/proyectoartistas/repository/ArtistasRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dark.proyectoartistas.repository;
-
-import com.dark.proyectoartistas.entity.Artistas;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArtistasRepository extends JpaRepository<Artistas, Long> {
-}
